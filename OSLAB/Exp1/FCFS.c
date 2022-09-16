31cbf345049ef37ffbb97a937cd2ff463335c693
204G1A05A6 <99065010+204G1A05A6@users.noreply.github.com>
2022-09-16 15:31:50 +0530

    File is created

---
+++ OSLAB/Exp1/FCFS.c
@@ -0,0 +1,10 @@
+/* Name Of the Candidate: C.Sushmitha
+   Roll No: 204G1A05A6
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 16-09-2022
+*/
+#include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
